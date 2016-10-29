29b210139346f4fcc13b30a21a25ab00c7c1d2d5
Maxym Lutsyuk <GettingStartedMax@gmail.com>
2016-10-29 21:10:01 +0400

    header.php Added. If user not logged - show login button, else - show nickname and logout button

--- appearance/detach/header.php
+++ appearance/detach/header.php
@@ -4,25 +4,10 @@
 
 defined('_INCLUDE_') or die('Shit happens!');
 
+if(!isset($_SESSION["user_nickname"])) {
+	include("logintab.php");
+}
+else {
+	echo("<span class='right'>Привіт, ".$_SESSION["user_nickname"].".<a href='?logout=1'>Вихід</a></span>");
+}
 ?>
-
-<div id='h_content'>
-	<div id='login_button_align'><button type='button' class='l_button' id='login_button' onClick='$("#alert").overlay().load();'>Увійти</button></div>
-	<div class='overlay' id='alert'>
-		<h2><center>Вхід</center></h2>
-		<p>
-			<form action='testreg.php' method='post'>
-				<p><input type='text' name='login' value='' placeholder='E-mail'></p>
-				<p><input type='password' name='password' value='' placeholder='Пароль'></p>
-				<p class='remember_me'>
-				<label>
-						<input type='checkbox' name='remember_me' id='remember_me'>
-						Запам'ятати мене
-					</label>
-				</p>
-				<p class='submit'><input type='submit' name='commit' value='Увійти'></p>
-				<div id='reg_href'><a href='reg.php'>Зареєструватися</a></div>
-			</form>
-		</p>
-	</div> 
-</div>
