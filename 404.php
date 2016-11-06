49268d0f9c657cce2847bb1e6999995ad1a84741
Maxym Lutsyuk <GettingStartedMax@gmail.com>
2016-11-06 20:50:13 +0200

    404.php Change: Created appearance of page 404; .htaccess Change: Corrected encoding appearance/images: Remove redundant files main-style.php Change: Added styles for page 404

--- 404.php
+++ 404.php
@@ -2,11 +2,9 @@
 //Файл: 404.php
 //Призанчення: файл на який перенаправляється у вмпадку помилки 404
 
-printf("
-	<p align='center'>
-		<img src='/appearance/images/404/8.gif'>
-	</p>
-	");
+define( '_INCLUDE_', 1 );
+
+include("/system/data/site_data.php")
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -14,8 +12,21 @@ printf("
 	<head>
 		<title>Ой, 404!</title>
 		<link href='appearance/css/main-style.css' rel='stylesheet' type='text/css'>
+		<link rel='shortcut icon' href='appearance/images/favicon.png' type='image/x-icon'>
+		<?php include('appearance/detach/scripts.php'); ?>
 	</head>
 	<body>
-		<strong>Sorry! Схоже що це помилка 404!</strong>
+		<?php
+			include("appearance/detach/header.php");
+			include("appearance/detach/glidemenu.php");
+		?>
+		<p align='center'>
+			<img src='/appearance/images/404/12.png'>
+		</p>
+		<h1 class='center' id='title_404'>Sorry! Схоже що це помилка 404!</h1>
+		<p class='center' id='text_404'>
+			Ми подзвонили Холмсу, він вже в дорозі.</br>
+			А поки ви можете <a href='<?php echo($site_address);?>'>повернутися на головну сторінку</a>
+		</p>
 	</body>
 </html>
